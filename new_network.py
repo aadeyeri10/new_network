ee58fe81535eb101d4d8c803a6aeecf9c5e28c62
Ayo Adeyeri <aadeyeri@terpmail.umd.edu>
2023-10-28 18:39:21 -0400

    add new function

---
+++ new_network.py
@@ -0,0 +1,9 @@
+def say_hello():
+    print("Hello, World!")
+
+def say_custom_greeting(greeting):
+    print(greeting)
+
+if __name__ == "__main__":
+    say_hello()
+    say_custom_greeting("Good morning!")
